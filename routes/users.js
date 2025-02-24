868fbeccfb88d45fd4359bc40f6f327066ea819b
Horlando Leao <horlandoleao8@gmail.com>
2025-02-24 00:21:59 -0300

    update dev

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get('/users/manage', async (req, res) => {
         const users = await User.findAll({
             include: UserConfig
         });
-        res.render('users/index', { title: 'Usuários', users, user: { name: req.session.user.name } });
+        res.render('users/index', { title: 'Usuários', users, user: { name: req.session.user.name }, novaSenhaAlerta: req.session.novaSenhaAlerta });
     } catch (error) {
         console.log(error)
         res.status(500).send('Erro ao buscar usuários');
@@ -41,12 +41,13 @@ router.post('/users/update/:id', async (req, res) => {
 });
 
 // Resetar senha
-router.post('/users/reset-auth/:id', async (req, res) => {
+router.get('/users/reset-auth', async (req, res) => {
     try {
-        const { email } = req.body;
+        const email  = req.query.email;
         const response = await User.findOne({ where: { email: email } });
-        response;
-    
+        const novaSenha = gerarSenha();
+        req.session.novaSenhaAlerta = `Usuário: ${email} | Nova senha: ${novaSenha}`
+        await User.update({ password:novaSenha }, { where: { id: response.dataValues.id } });
         // codigo que enviar o email
         
         res.redirect('/users/manage');
@@ -54,15 +55,5 @@ router.post('/users/reset-auth/:id', async (req, res) => {
         res.status(500).send('Erro ao atualizar usuário');
     }
 });
-
-// Deleta um usuário
-// router.post('/users/delete/:id', async (req, res) => {
-//     try {
-//         await User.destroy({ where: { id: req.params.id } });
-//         res.redirect('/users');
-//     } catch (error) {
-//         res.status(500).send('Erro ao deletar usuário');
-//     }
-// });
 
 module.exports = router;
