fd57c016ff5867d91ae28b1bd36dfe4b65c81230
Horlando Leao <horlandoleao8@gmail.com>
2025-03-21 00:20:37 -0300

    removendo codigo ligo

--- cronjobs/cronjobs.js
+++ cronjobs/cronjobs.js
@@ -1,21 +1,7 @@
-const path = require('path');
-const { removeAllFiles } = require("../helpers/removeAllFiles");
 const { cronManager } = require("../infra/cronManager/index");
-const { objectStorage } = require("../infra/objectStorage/index");
-const { config } = require('../config/config');
 
-const taskBackup = () => {
-    const dumpFolder =  path.join(__dirname, '..', 'backups');
-    const dumpFile =  path.join(dumpFolder, `backup_${config.database}_${Date.now()}.sql`);
-
-    cronManager.addJob("apagar-todos-backups", "0 0 * * *", () => removeAllFiles(dumpFolder), false)
-    cronManager.addJob("salvar-backup-mysql", "0 2 * * *", () => objectStorage.upload(dumpFile))
-
-    // cronManager.startJob("apagar-todos-backups");
-    // cronManager.startJob("gerar-backup-mysql");
-    // cronManager.startJob("salvar-backup-mysql");
-}
-taskBackup()
+cronManager.addJob("teste", "0 0 * * *", () => console.log("Teste de cron"), false)
+cronManager.startJob("teste")
 
 console.log("Todas as cron em execução: ", cronManager.listJobsRunning());
 
